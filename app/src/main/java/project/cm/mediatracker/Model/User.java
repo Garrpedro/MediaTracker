1dc8dd2c9dde8fce0e316efb2bdff835571f6a98
garrpedro <garrpedro@gmail.com>
2017-12-26 14:56:08 +0000

    User Done!!!

--- app/src/main/java/project/cm/mediatracker/Model/User.java
+++ app/src/main/java/project/cm/mediatracker/Model/User.java
@@ -1,5 +1,7 @@
 package project.cm.mediatracker.Model;
 
+import java.util.Date;
+
 /**
  * Created by pedrog on 26-12-2017.
  */
@@ -7,5 +9,57 @@ package project.cm.mediatracker.Model;
 public class User {
 
     private String email;
+    private String username;
+    private String password;
+    private Date birthDate;
 
+
+    public User(String email, String username, String password, Date birthDate) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
 }
