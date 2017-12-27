b34834d301350db88dd941a91820d8b2890ef16f
Francisco Guerra <franciscojpguerra@gmail.com>
2017-12-27 16:19:43 +0000

    Layout Files Created

---
+++ app/src/main/java/project/cm/mediatracker/SerieAnimeActivity.java
@@ -0,0 +1,13 @@
+package project.cm.mediatracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SerieAnimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_serie_anime);
+    }
+}
