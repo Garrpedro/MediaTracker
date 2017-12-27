53369e0e5e81e7737381bbfa1b165c50616046a8
Francisco Guerra <franciscojpguerra@gmail.com>
2017-12-27 16:05:54 +0000

    Started Layout Work

--- app/src/main/java/project/cm/mediatracker/ForgotPasswordActivity.java
+++ app/src/main/java/project/cm/mediatracker/ForgotPasswordActivity.java
@@ -3,12 +3,11 @@ package project.cm.mediatracker;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class ForgotPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_forgot_password);
     }
 }
