4e70aea5c0a832754cc235e1ddd09410e54cae72
Francisco Guerra <franciscojpguerra@gmail.com>
2017-12-29 11:39:43 +0000

    Toolbar Added

--- app/src/main/java/project/cm/mediatracker/HomeActivity.java
+++ app/src/main/java/project/cm/mediatracker/HomeActivity.java
@@ -2,23 +2,56 @@ package project.cm.mediatracker;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatCallback;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.view.ActionMode;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
-public class HomeActivity extends Activity {
-
+public class HomeActivity extends Activity implements AppCompatCallback {
+    private AppCompatDelegate delegate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        setTheme(android.R.style.Theme_Holo);
-        //getSupportActionBar().hide();
+        //let's create the delegate, passing the activity at both arguments (Activity, AppCompatCallback)
+        delegate = AppCompatDelegate.create(this, this);
+
+        //we need to call the onCreate() of the AppCompatDelegate
+        delegate.onCreate(savedInstanceState);
+
+        //we use the delegate to inflate the layout
+        delegate.setContentView(R.layout.activity_home);
+
+        //Finally, let's add the Toolbar
+        Toolbar toolbar = findViewById(R.id.tool_bar);
+        delegate.setSupportActionBar(toolbar);
+        delegate.getSupportActionBar().setTitle(null);
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu_web_view; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
+    }
 
+
+    @Override
+    public void onSupportActionModeStarted(ActionMode mode) {
+        //let's leave this empty, for now
+    }
+
+    @Override
+    public void onSupportActionModeFinished(ActionMode mode) {
+        // let's leave this empty, for now
+    }
+
+    @Nullable
+    @Override
+    public ActionMode onWindowStartingSupportActionMode(ActionMode.Callback callback) {
+        return null;
     }
 }
