f3a0d2b58aa64284caede60a21248eaec5382649
MASTER2 <isoares@live.com.pt>
2017-12-29 15:08:01 +0000

    user login

--- app/src/main/java/project/cm/mediatracker/LoginActivity.java
+++ app/src/main/java/project/cm/mediatracker/LoginActivity.java
@@ -1,13 +1,79 @@
 package project.cm.mediatracker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    EditText editText_email;
+    EditText editText_password;
+    Button btn_login;
+    Button btn_create_acc;
+    Button btn_forgot_pass;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editText_email = findViewById(R.id.editText_email);
+        editText_password = findViewById(R.id.editText_password);
+        btn_login = findViewById(R.id.btn_login);
+        btn_create_acc= findViewById(R.id.btn_create_acc);
+        btn_forgot_pass = findViewById(R.id.btn_forgot_pass);
+
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+
+    void login () {
+
+        mAuth.signInWithEmailAndPassword(editText_email.getText().toString(), editText_password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                            Intent intent=new Intent(LoginActivity.this,HomeActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
