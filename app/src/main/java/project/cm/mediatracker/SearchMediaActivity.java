62af6072df06391535c9d304f51e69aa739f9cd7
garrpedro <garrpedro@gmail.com>
2018-01-06 17:24:34 +0000

    Deleted MoviesActivity and SerieAnimeActivity and added the MediaContentActivity

--- app/src/main/java/project/cm/mediatracker/SearchMediaActivity.java
+++ app/src/main/java/project/cm/mediatracker/SearchMediaActivity.java
@@ -19,79 +19,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class SearchMediaActivity extends AppCompatActivity {
-    String olateste = "fodasse";
-
-    TextView txtViewTitleSearch, txtViewYearSearch, txtViewGenreSearch, txtViewReleasedSearch, txtViewPlotSearch;
-    ImageView imgViewPosterSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_media);
-
-        txtViewTitleSearch = findViewById(R.id.txtViewTitleSearch);
-        txtViewYearSearch = findViewById(R.id.txtViewYearSearch);
-        txtViewGenreSearch = findViewById(R.id.txtViewGenreSearch);
-        txtViewReleasedSearch = findViewById(R.id.txtViewReleasedSearch);
-        txtViewPlotSearch = findViewById(R.id.txtViewPlotSearch);
-
-        imgViewPosterSearch = findViewById(R.id.imgViewPosterSearch);
-
-        SearchMediaContent();
-
-        olateste = getIntent().getStringExtra("media_type");
-    }
-
-    public  void  SearchMediaContent()
-    {
-        // Send data
-        try
-        {
-            String url = "http://www.omdbapi.com/?apikey=675629b3&i=";
-
-            JsonObjectRequest jsonRequest = new JsonObjectRequest
-                    (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            // the response is already constructed as a JSONObject!
-                            try {
-                                //response = response.getJSONObject("args");
-                                String title = response.getString("Title");
-                                String year = response.getString("Year");
-                                String genre = response.getString("Genre");
-                                String released = response.getString("Released");
-                                String plot = response.getString("Plot");
-                                String poster = response.getString("Poster");
-
-                                txtViewTitleSearch.setText(title);
-                                txtViewYearSearch.setText(year);
-                                txtViewGenreSearch.setText(genre);
-                                txtViewReleasedSearch.setText(released);
-                                txtViewPlotSearch.setText(plot);
-
-                                if (!poster.equals("N/A"))
-                                    Picasso.with(getApplicationContext()).load(poster).into(imgViewPosterSearch);
-                                else
-                                    imgViewPosterSearch.setImageResource(R.drawable.missing_image);
-
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }, new Response.ErrorListener() {
-
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            error.printStackTrace();
-                        }
-                    });
-            Volley.newRequestQueue(this).add(jsonRequest);
-        }
-        catch(Exception ex)
-        {
-        }
-        finally
-        {
-        }
     }
 }
